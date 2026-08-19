ab054466ebc0717c770795bf04e673865bba196e
wooch123 <wooch123@gmail.com>
2026-08-19 21:05:15 +0900

    chore: 임시 요청 스크립트 제거

--- scripts/_tmp-post.mjs
+++
@@ -1,7 +0,0 @@
-const [,, actionId, valuesJson] = process.argv;
-const res = await fetch('http://127.0.0.1:3000/api/runtime/action', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json; charset=utf-8' },
-  body: JSON.stringify({ actionId, context: { componentValues: JSON.parse(valuesJson), routeParams: JSON.parse(process.argv[5] ?? '{}') } }),
-});
-console.log(res.status, await res.text());
